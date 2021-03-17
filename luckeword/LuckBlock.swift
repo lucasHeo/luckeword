50498fa64c370226b8e4596c4a885eb901a01559
lucasHeo <hsj02036@gmail.com>
2021-03-17 21:47:29 +0900

    v0.0.1 : layout Setting

---
+++ luckeword/LuckBlock.swift
@@ -0,0 +1,16 @@
+//
+//  LuckBlock.swift
+//  luckeword
+//
+//  Created by Heo on 2021/03/17.
+//
+
+import UIKit
+
+class LuckBlock: UIView {
+    
+    required init?(coder aDecoder: NSCoder) {
+            super.init(coder: aDecoder)
+        self.layer.cornerRadius = 10
+    }
+}
--- luckeword/ViewController.swift
+++ luckeword/ViewController.swift
@@ -8,10 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    let score: Int = 0
+    
+    @IBOutlet weak var keywordLabel: UILabel!
+    let keyword: String = ""
+    
+    @IBOutlet weak var colorView: UIView! {
+        didSet {
+            colorView.layer.cornerRadius = 18
+        }
+    }
+    let colorName: UIColor = UIColor.white
+    
+    @IBOutlet weak var scoreDateLabel: UILabel!
+    @IBOutlet weak var keywordDateLabel: UILabel!
+    @IBOutlet weak var colorDateLabel: UILabel!
+    
+    @IBOutlet weak var versionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        scoreLabel.text = "100"
+        keywordLabel.text = "행복"
+        colorView.layer.backgroundColor = UIColor.black.cgColor
     }
 
 
